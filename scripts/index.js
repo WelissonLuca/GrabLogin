d8ada4d4e8c394606d5989d3784536db38c1a21f
Welisson Luca <welissonluca17@gmail.com>
2021-01-12 10:59:47 -0300

    AddScript

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const inputEmail = document.getElementById("name");
+const inputPassword = document.getElementById("password");
+console.log(inputEmail, inputPassword);
+
+function checkedEmail() {
+    const check = inputEmail.value
+    console.log(check)
+}
